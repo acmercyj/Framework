1b2d213b3c1788d326fa9cfd4160107a86215044
zhengchenyang <690574486@qq.com>
2018-09-27 22:17:56 +0800

    mac版转表

--- Excels/go-mac.sh
+++ Excels/go-mac.sh
@@ -6,7 +6,7 @@ do
 echo $xls
 sheet=${xls:4:${#xls}-8}"Table"
 # SHEET=$(echo $sheet | tr '[a-z]' '[A-Z]')
-python tool/xls_deploy_tool.py $sheet $xls
+python tool/xls_deploy_tool_mac.py $sheet $xls
 mono protogen/bin/protogen.exe -i:"proto/"$sheet".proto" -o:"../Assets/Scripts/Sheet/"$sheet".cs"
 done
 echo
